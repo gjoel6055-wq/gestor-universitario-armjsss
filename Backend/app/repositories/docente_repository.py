e392d22c02de06b64c5700e9a9178c6872bb22a8
sophnegri <sophinegri@gmail.com>
2026-05-20 11:14:41 -0300

    feat: CRUD completo de Docentes con borrado lógico

---
+++ Backend/app/repositories/docente_repository.py
@@ -0,0 +1,53 @@
+from app.db import get_db_connection 
+
+def obtener_docentes_db():
+    conexion = get_db_connection()
+    cursor = conexion.cursor(dictionary=True) 
+    
+    try:
+        query = """
+            SELECT d.legajo, d.departamento, u.nombre, u.apellido, u.email 
+            FROM docentes d
+            INNER JOIN usuarios u ON d.usuario_id = u.usuario_id
+            WHERE d.deleted_at IS NULL
+        """
+        cursor.execute(query)
+        return cursor.fetchall()
+    except Exception as e:
+        raise e
+    finally:
+        cursor.close()
+        conexion.close()
+
+def crear_docente_db(legajo, usuario_id, departamento):
+    conexion = get_db_connection()
+    cursor = conexion.cursor()
+    
+    try:
+        query = "INSERT INTO docentes (legajo, usuario_id, departamento) VALUES (%s, %s, %s)"
+        valores = (legajo, usuario_id, departamento)
+        cursor.execute(query, valores)
+        conexion.commit()
+        return legajo 
+    except Exception as e:
+        conexion.rollback()
+        raise e
+    finally:
+        cursor.close()
+        conexion.close()
+
+def eliminar_docente_db(legajo):
+    conexion = get_db_connection()
+    cursor = conexion.cursor()
+    
+    try:
+        query = "UPDATE docentes SET deleted_at = CURRENT_TIMESTAMP WHERE legajo = %s"
+        cursor.execute(query, (legajo,))
+        conexion.commit()
+        return cursor.rowcount > 0
+    except Exception as e:
+        conexion.rollback()
+        raise e
+    finally:
+        cursor.close()
+        conexion.close()
