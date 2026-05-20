01c8c91fa35b5b780bacacda370554d3d87d873a
sophnegri <sophinegri@gmail.com>
2026-05-20 17:20:58 -0300

    chore: vaciar contenido de init.sh

--- Backend/init.sh
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from app.routers.material_routes import material_bp # Importas tu blueprint
-
-app = Flask(__name__)
-
-app.register_blueprint(material_bp)
-
-if __name__ == '__main__':
-    app.run(debug=True)
