9c9c3b93276c6981f2f3cb2edb3be728559ac272
amichaluk-hub <amichaluk@fi.uba.ar>
2026-05-20 17:38:35 -0300

    arme la estructura del service de evaluaciones para conectar con las rutas

---
+++ Backend/app/services/evaluacion_service.py
@@ -0,0 +1,8 @@
+def crear_evaluacion_servicio(datos):
+    return datos
+
+def modificar_evaluacion_servicio(id_evaluacion, datos):
+    return True
+
+def borrar_evaluacion_servicio(id_evaluacion):
+    return True
