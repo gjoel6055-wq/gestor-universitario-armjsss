398b2c0021135fd4d922e0460c6d905091990d7f
gjoel6055-wq <gjoel6055@gmail.com>
2026-05-20 17:04:13 -0300

    registrar blueprints de auth y asistencia y configurar seguridad de sesiones

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -2,10 +2,17 @@ from flask import Flask
 from app.routes.cursos import cursos_bp
 from app.routes.equipos import equipos_bp
 from app.routes.docentes import docente_bp
+from app.routes.auth import auth_bp
+from app.routes.asistencia import asistencia_bp
 
 def create_app():
     app = Flask(__name__)
 
+    app.secret_key = 'una_clave_de_prueba_secreta_y_dificil_ARMJSSS_2026'
+    app.config['SESSION_COOKIE_HTTPONLY'] = True
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(asistencia_bp)
     app.register_blueprint(cursos_bp)
     app.register_blueprint(equipos_bp)
     app.register_blueprint(docente_bp)
