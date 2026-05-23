edbae70ea9ffa0bdca600d03e0f2fd34f0c610b8
amichaluk-hub <amichaluk@fi.uba.ar>
2026-05-23 11:42:36 -0300

    correción de errores

--- Backend/app/routes/tipos_evaluacion.py
+++ Backend/app/routes/tipos_evaluacion.py
@@ -1,13 +1,41 @@
 from flask import Blueprint, request, jsonify
+from app.services.tipo_evaluacion_service import (
+     crear_tipo_servicio,
+     modificar_tipo_servicio,
+     borrar_tipo_servicio
+)
 
 tipos_evaluacion_bp = Blueprint('tipos_evaluacion', __name__)
 
 @tipos_evaluacion_bp.route('/tipos-evaluacion', methods=['POST'])
 def crear_tipo_evaluacion():
     datos = request.get_json()
-    nombre = datos.get('nombre') # Ej: "Primer Parcial"
+    nombre = datos.get('nombre')
     
     if not nombre:
-        return jsonify({'error': 'El nombre del tipo de evaluación es requerido.'}), 400
-        
-    return jsonify({'mensaje': f'Tipo de evaluación "{nombre}" creado con éxito.'}), 201 
+        return jsonify({'error': 'El campo nombre es obligatorio'}), 400
+
+    resultado = crear_tipo_servicio(datos)
+    return jsonify({'mensaje': 'Tipo de evaluación creado con éxito.','datos': resultado}), 201 
+
+@tipos_evaluacion_bp.route('/tipos-evaluacion/<int:id>', methods=['PUT', 'DELETE'])
+def gestionar_tipo_evaluacion(id):
+    if request.method == 'PUT':
+        datos = request.get_json()
+        nombre = datos.get('nombre')
+
+        if not nombre:
+            return jsonify ({'error': 'El campo nombre es obligatorio para actualizar'}), 400
+
+        encontrado = modificar_tipo_servicio(id,datos)
+        if not encontrado:
+            returno jsonify ({'error': f'No se encontró el tipo de evaluación con ID {id}'}), 404
+
+        return jsonify ({'mensaje': f'Tipo de evaluación {id} actualizada con éxito.'}), 200
+
+if request.method == 'DELETE':
+    encontrado=borrar_tipo_servicio(id)
+    if not encontrado:
+        return jsonify({'error': f'No se encontró el tipo de evaluación con ID {id}'}), 404
+
+    return jsonify({'mensaje': f'Tipo de evaluación {id} eliminado correctamente'}), 200
