3444ff41898a1d37c8160283e6de25ffcf7595fe
amichaluk-hub <amichaluk@fi.uba.ar>
2026-05-23 11:01:55 -0300

    modifique el codigo para que conecte a la base de datos, y arregle unos errores

--- Backend/app/routes/evaluaciones.py
+++ Backend/app/routes/evaluaciones.py
@@ -1,4 +1,9 @@
 from flask import Blueprint, request, jsonify
+from app.services.evaluacion_service import (
+     crear_evaluacion_servicio,
+     modificar_evaluacion_servicio,
+     borrar_evaluacion_servicio
+)
 
 evaluaciones_bp = Blueprint('evaluaciones', __name__)
 
@@ -7,15 +12,21 @@ def crear_evaluacion():
     datos = request.get_json()
     id_tipo = datos.get('id_tipo')
     fecha = datos.get('fecha')
-    
-    
-    return jsonify({'mensaje': 'Evaluación programada con éxito.'}), 201
+    id_curso = datos.get ('id_curso')
+
+    if not id_tipo or not fecha or not id_curso:
+        return jsonify({'error':'Faltan datos obligatorios'}), 400
+        
+    resultado = crear_evaluacion_servicio (datos) 
+    return jsonify({'mensaje': 'Evaluación programada con éxito.', 'datos': resultado}), 201
 
 @evaluaciones_bp.route('/evaluaciones/<int:id>', methods=['PUT', 'DELETE'])
 def gestionar_evaluacion(id):
     if request.method == 'PUT':
         datos = request.get_json()
+        modificar_evaluacion_servicio (id, datos)
         return jsonify({'mensaje': f'Evaluación {id} actualizada con éxito.'}), 200
         
     if request.method == 'DELETE':
+        borrar_evaluacion_servicio (id)
         return jsonify({'mensaje': f'Evaluación {id} eliminada correctamente.'}), 200
