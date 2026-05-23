88f24fb56e29e7ea1c24769d5789fe174b6fd428
amichaluk-hub <amichaluk@fi.uba.ar>
2026-05-23 12:06:44 -0300

    agregue funciones de simulación

--- Backend/app/repositories/tipo_evaluacion_repository.py
+++ Backend/app/repositories/tipo_evaluacion_repository.py
@@ -1,4 +1,17 @@
-def guardar_tipo_evaluacion_bd(nombre):
-    print(f"Base de Datos: Guardando nuevo tipo de evaluacion: {nombre}")
-    
-    return {"id": 1, "nombre": nombre}
+def guardar_tipo_bd(datos):
+    print(f"Base de Datos: Guardando tipo de evaluación... {datos}")
+    datos_guardados = datos.copy()
+    datos_guardados['id'] = 1  
+    return datos_guardados
+
+def modificar_tipo_bd(id, datos):
+    print(f"Base de Datos: Modificando tipo de evaluación con ID {id}")
+    if id > 500:
+        return False
+    return True
+
+def borrar_tipo_bd(id):
+    print(f"Base de Datos: Eliminando tipo de evaluación con ID {id}")
+    if id > 500:
+        return False
+    return True
