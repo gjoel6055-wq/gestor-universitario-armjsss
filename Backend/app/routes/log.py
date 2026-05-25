3c26b2aed9cc4d9fcc5944464d0430b6fd1b27f5
gjoel6055-wq <gjoel6055@gmail.com>
2026-05-25 16:39:19 -0300

    Se implementa la logica de registro e historial de logs

--- Backend/app/routes/log.py
+++ Backend/app/routes/log.py
@@ -1,9 +1,11 @@
 from flask import request, Blueprint, jsonify
 from app.services.log_service import listar_registro_actividad, log_por_id
+from app.services.auth_service import requiere_token
 
 log_bp = Blueprint("log", __name__)
 
 @log_bp.route('/historial_logs')
+@requiere_token()
 def listar_logs():
     historial = listar_registro_actividad()
 
@@ -13,10 +15,11 @@ def listar_logs():
     return jsonify({'historial': historial}), 200
 
 @log_bp.route('/logs/<int:id_log>')
+@requiere_token()
 def buscar_log(id_log):
     log_buscado = log_por_id(id_log)
 
     if log_buscado == "no existe log con ese id":
-        return jsonify({'error':"No se encontro un log con ese id."}), 404
+        return jsonify({'error':"No se encontró un log con ese id."}), 404
 
     return  jsonify({"log": log_buscado}), 200
